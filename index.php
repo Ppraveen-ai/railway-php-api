279642d511b45cb74ca201c8ee38c482be6eb994
Ppraveen-ai <jnp.praveen@gmail.com>
2025-09-14 23:53:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
+echo "Hello from Railway PHP!";
+?>
 
-echo json_encode([
-    "status" => "ok",
-    "message" => "PHP API is running",
-    "endpoints" => [
-        "POST /api.php" => "Insert complaint data into complaints_table"
-    ]
-]);
+
